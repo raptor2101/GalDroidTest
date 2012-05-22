31793dfe9d7ea5c0acc19f1749d397bbba5ce01c
Raptor 2101 <raptor2101@gmx.de>
2012-05-23 00:14:32 +0200

    CHG: override toString for better identification in assert-messages

--- src/de/raptor2101/GalDroid/Testing/ComponentTest/Activities/TestImplementations/TestGalleryObject.java
+++ src/de/raptor2101/GalDroid/Testing/ComponentTest/Activities/TestImplementations/TestGalleryObject.java
@@ -56,6 +56,10 @@ public class TestGalleryObject implements GalleryObject {
 	}
 	
 	@Override
+	public String toString() {
+		return mObjectId;
+	}
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
